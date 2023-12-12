db2681b3dd3b66f99c228943706f074e3edc7728
TeachMeHacking <147134853+TeachMeHacking@users.noreply.github.com>
2023-12-12 10:56:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ from flask import Flask,request
 
 app=Flask(__name__)
 
-@app.route('/')
-def nooo():
-    return "Welcome L"
+
+global url1 
+
+@app.route('/postUrl', methods=['POST'])
+def postUrl():
+    url = request.args.get("url")
+    url = url
+    return url
+
 
 @app.route('/getUrl', methods=['GET'])
-def home():
-    return "ok Done"
-
+def getUrl():
+    Url = {}
+    if url1 == None:
+        Url = {
+            "Url" : "Null",
+            "Success" : -1,
+            "Error Code" : -1
+               }
+    else:
+           Url = {
+            "Url" : url1,
+            "Success" : 0,
+            "Error Code" : 0
+            }
+    return Url
 
 if __name__ == "__main__":
     app.run()#(debug=False,host='0.0.0.0')
